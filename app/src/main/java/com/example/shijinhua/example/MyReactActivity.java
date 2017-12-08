8bfe759a3ec0b78000cd68701ca824f03de713fd
shijinhua <shijinhua@yatang.cn>
2017-12-08 18:23:49 +0800

    init

---
+++ app/src/main/java/com/example/shijinhua/example/MyReactActivity.java
@@ -0,0 +1,16 @@
+import android.support.annotation.Nullable;
+
+import com.facebook.react.ReactActivity;
+
+/**
+ * Created by Song on 2017/2/13.
+ */
+public class MyReactActivity extends ReactActivity {
+
+    @Nullable
+    @Override
+    protected String getMainComponentName() {
+        return "HotRN";
+    }
+
+}
